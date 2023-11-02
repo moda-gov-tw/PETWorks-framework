dcc9d9240a2be6fe2963ec52d9e3fc100e1f6e4c
haeter525 <cindejze412@gmail.com>
2023-11-02 15:24:29 +0800

    Fix unittest

--- tests/differential_privacy/test_MIATester.py
+++ tests/differential_privacy/test_MIATester.py
@@ -1,7 +1,7 @@
 import pytest
 import PETWorks.differential_privacy.MIATester as MIATester
 
-# @pytest.mark.skip(reason="Take too much time that GitHub action can tolerate. (Over than 30 mins)")
+@pytest.mark.skip(reason="Take too much time that GitHub action can tolerate. (Over than 30 mins)")
 def testPETValidation():
     synthetic = "data/synthetic_NHANES.csv"
     original = "data/NHANES.csv"
